44ea6ecc8ea907ddfffd3bf0b5c5bdfec9ef8eee
FrancescoBuonomo <buonomofrancescow@gmail.com>
2023-11-02 15:33:02 +0100

    add some while try

--- js/main.js
+++ js/main.js
@@ -1,29 +1,41 @@
-console.log('funziona', axios);
+// console.log('funziona', axios);
 
 const { createApp } = Vue
 
   createApp({
     data() {
       return {
-        message: 'Hello Vue!',
-        email:''
+        message: 'Lista di email',
+        emails:[],
+        i : 0,
       }
     },
     methods:{
+        
         fetchEmail(){
-            axios
-                .get('https://flynn.boolean.careers/exercises/api/random/mail')
-                .then((res) =>{
-                    console.log(res)
-                    const newEmail = res.data.response
-                    this.email = newEmail
-                    
-                })
+            //   while(this.emails.lenght < 10){
+                
+                  axios
+                      .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                      .then((res) =>{
+                          // console.log(res);
+                          const newEmail = res.data.response;
+                          
+                          this.emails.push(newEmail);
+                          
+                          
+                        })
+                        console.log(this.emails);
+            //   }
         }
 
     },
     created() {
+        // while(this.emails.lenght < 10){
         this.fetchEmail()
+
+        // console.log(emails) } 
+        
     }
   }).mount('#app')
 
