9b022e61c58a0d65ee03d301b25a3b8fde27c430
FrancescoBuonomo <buonomofrancescow@gmail.com>
2023-11-02 16:07:44 +0100

    add loop for

--- js/main.js
+++ js/main.js
@@ -2,42 +2,35 @@
 
 const { createApp } = Vue
 
-  createApp({
+createApp({
     data() {
-      return {
-        message: 'Lista di email',
-        emails:[],
-        i : 0,
-      }
-    },
-    methods:{
-        
-        fetchEmail(){
-            //   while(this.emails.lenght < 10){
-                
-                  axios
-                      .get('https://flynn.boolean.careers/exercises/api/random/mail')
-                      .then((res) =>{
-                          // console.log(res);
-                          const newEmail = res.data.response;
-                          
-                          this.emails.push(newEmail);
-                          
-                          
-                        })
-                        console.log(this.emails);
-            //   }
+        return {
+            message: 'Lista di email',
+            emails: [],
+            i: 0,
         }
-
+    },
+    methods: {
+        fetchEmail() {
+            for (let i = 0; i < 10; i++) {
+                axios
+                    .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((res) => {
+                        const newEmail = res.data.response;
+                        this.emails.push(newEmail);
+                        console.log(this.emails);
+                    }, (err) => {
+                        console.log(err);
+                    });
+            }
+        }
     },
     created() {
-        // while(this.emails.lenght < 10){
         this.fetchEmail()
-
-        // console.log(emails) } 
-        
+    },
+    mounted() {
     }
-  }).mount('#app')
+}).mount('#app')
 
 
 
