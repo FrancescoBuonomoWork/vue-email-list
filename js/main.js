dde83ef2093a8b7d8e27e55c4da610a63add3c65
FrancescoBuonomo <buonomofrancescow@gmail.com>
2023-11-03 10:01:12 +0100

    add num

--- js/main.js
+++ js/main.js
@@ -11,8 +11,8 @@ createApp({
         }
     },
     methods: {
-        fetchEmail() {
-            for (let i = 0; i < 10; i++) {
+        fetchEmail(num) {
+            for (let i = 0; i < num; i++) {
                 axios
                     .get('https://flynn.boolean.careers/exercises/api/random/mail')
                     .then((res) => {
@@ -26,7 +26,7 @@ createApp({
         }
     },
     created() {
-        this.fetchEmail()
+        this.fetchEmail(10)
     },
     mounted() {
     }
